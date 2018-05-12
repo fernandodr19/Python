866d0aabcf339a864841eb3c66800438ef84f7c4
Fernando Da Rós Filho <fdarosfilho@gmail.com>
2018-05-12 16:01:20 -0300

    Add youtube match

---
+++ youtube.py
@@ -0,0 +1,18 @@
+import webbrowser
+import requests
+from bs4 import BeautifulSoup
+
+query = input('Enter the song to be played: ')
+query = query.replace(' ', '+')
+
+# search for the best similar matching video
+url = 'https://www.youtube.com/results?search_query=' + query
+source_code = requests.get(url,timeout=15)
+plain_text = source_code.text
+soup = BeautifulSoup(plain_text, "html.parser")
+
+# fetches the url of the video
+songs = soup.findAll('div', {'class': 'yt-lockup-video'})
+song = songs[0].contents[0].contents[0].contents[0]
+link = song['href']
+webbrowser.open('https://www.youtube.com' + link)
