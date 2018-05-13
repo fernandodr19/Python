3aea9e7aca071aef1454831adfb406aa3e242b0f
Fernando Da Rós Filho <fdarosfilho@gmail.com>
2018-05-13 12:02:08 -0300

    Small change

--- bar.py
+++ bar.py
@@ -5,6 +5,7 @@ objects = ('Label1', 'Label2', 'Label3', 'Label4', 'Label5', 'Label6')
 y_pos = np.arange(len(objects))
 performance = [10,8,6,4,7,1]
  
+plt.figure(figsize=(12,3))
 plt.bar(y_pos, performance, align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 plt.ylabel('Y axis')
